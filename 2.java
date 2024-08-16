cb30a232007c1c302634c8d54409a837f838a314
FarhhanSyed <163603911+FarhhanSyed@users.noreply.github.com>
2024-08-16 19:15:37 +0530

    Update and rename b.java to 2.java

--- 2.java
+++ 2.java
@@ -3,10 +3,10 @@
     **
     *
  */
-public class b{
-    public static void main(String args[]){
-        int n=4;
-        for(int i=1;i<=4;i++)
+public class 2{
+   public static void pattern(int n)
+      {
+         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=(n-i+1);j++)
             {
@@ -14,5 +14,8 @@ public class b{
             }
             System.out.println();
         }
+}
+    public static void main(String args[]){
+        pattern(4);
     }
 }
