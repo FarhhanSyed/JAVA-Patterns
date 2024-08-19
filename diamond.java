dd3dadcd7eecd4051431635dc5235afca599477a
FarhhanSyed <163603911+FarhhanSyed@users.noreply.github.com>
2024-08-19 17:55:56 +0530

    Add files via upload

---
+++ diamond.java
@@ -0,0 +1,48 @@
+/*
+       *
+      ***
+     *****
+    *******
+    *******
+     *****
+      ***
+       *
+ */
+public class diamond {
+    public static void pattern(int n)
+    {
+        //first-half
+        for(int i=1;i<=n;i++)
+        {
+            //spaces-(n-i)
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            //stars-(2i-1)
+            for(int j=1;j<=2*i-1;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //second-half
+        for(int i=n;i>=1;i--)
+        {
+            //spaces-(n-i)
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            //stars-(2i-1)
+            for(int j=1;j<=2*i-1;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pattern(4);
+    }
+}
