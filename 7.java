67fd75fa2d7e31e7515454110a2e7c4dbfb497f0
FarhhanSyed <163603911+FarhhanSyed@users.noreply.github.com>
2024-08-22 11:18:43 +0530

    Update and rename k.java to 7.java

--- 7.java
+++ 7.java
@@ -5,7 +5,7 @@
     0101
     10101
  */
-public class k {
+public class 7 {
     public static void pattern(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
